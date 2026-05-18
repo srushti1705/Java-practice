02a4c6694370386339076c917127a2011c1460c1
Srushti <srushtiwadi17@gmail.com>
2026-05-18 12:35:50 +0530

    Swapping 2 numbers using third variable

---
+++ swapping.java
@@ -0,0 +1,12 @@
+public class swapping {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+
+		int temp = a;
+		a = b;
+		b = temp;
+
+		System.out.println("Swapped numbers: " + a + ", " + b);
+ }
+}
