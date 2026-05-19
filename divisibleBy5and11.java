9d26806c47ab33d8773097c39b6487f24d064905
Srushti <srushtiwadi17@gmail.com>
2026-05-19 11:29:26 +0530

    Check if a number is divisible by 5 and 11

---
+++ divisibleBy5and11.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class divisibleBy5and11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a numbmer: ");
+        int num = sc.nextInt();
+
+        if (num % 5 == 0 && num % 11 == 0) {
+            System.out.println("Number is divisible by 5 and 11");
+        }
+        else {
+            System.out.println("Number is not divisible by 5 and 11");
+        }
+    }
+}
