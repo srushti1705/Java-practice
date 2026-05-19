9c252f0ab721615897874653c33a6a7cc794b2cb
Srushti <srushtiwadi17@gmail.com>
2026-05-19 22:45:37 +0530

    Simple calculator using switch case

---
+++ calculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.println("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Enter operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        double result = 0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator");
+                return;
+        }
+        System.out.println("Result: " + result);
+    }
+}
