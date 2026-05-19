9da654c73d8bcbaa1e0dfc175acc9baee49e1029
Srushti <srushtiwadi17@gmail.com>
2026-05-19 12:21:46 +0530

    Check the sum of first and last digit of a 3 digit number

---
+++ sumofFirstandLastdigit.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;  
+
+public class sumofFirstandLastdigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        int first = num / 100;
+        int last = num % 10;
+        sum = first + last;
+
+        System.out.println("Sum: " + sum);
+    }
+}
