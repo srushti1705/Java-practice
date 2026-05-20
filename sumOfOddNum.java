e229f457c8cf4c8463e2a7c85116e19288eae401
Srushti <srushtiwadi17@gmail.com>
2026-05-20 11:05:53 +0530

    Calculate the sum of odd numbers form 1 to n

---
+++ sumOfOddNum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class sumOfOddNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println("Sum of odd numbers from 1 to " + n + " is: " + sum);
+    }
+}
