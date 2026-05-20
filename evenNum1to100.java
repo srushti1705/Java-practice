22780f32ff47a0e533ac81aa3bebb3f2acfd9ef1
Srushti <srushtiwadi17@gmail.com>
2026-05-20 10:58:27 +0530

    Print even numbers from 1 to 100 using while loop

---
+++ evenNum1to100.java
@@ -0,0 +1,12 @@
+public class evenNum1to100 {
+    public static void main(String[] args) {
+        int num = 1;
+
+        while (num <= 100) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+            num++;
+        }
+    }
+}
