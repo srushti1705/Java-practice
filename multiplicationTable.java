2dca6be0aefe37670fabdd76ad220ec51a40d2be
Srushti <srushtiwadi17@gmail.com>
2026-05-20 11:10:18 +0530

    Print multiplication table of a number

---
+++ multiplicationTable.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class multiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+            i++;
+        }
+    }
+}
